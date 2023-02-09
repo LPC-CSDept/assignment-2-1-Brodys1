c123712bca81896579d679a99843843cd06d6466
Brodys1 <123720584+Brodys1@users.noreply.github.com>
2023-02-08 18:08:14 -0800

    assignment 2

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
     ##################################################
     # Comlete your code here
+    men = int(input('Number of male indentifying students : '))
+    women = int(input('Number of female indentifying students : '))
+    students = int(women + men)
+    per_males = (men/students)*100
+    per_females = (women/students)*100
+    print(f'\nThe total number of students:\t\t{students}\
+        \nThe number of males and females:\t {men}\t {women}\
+        \nThe percentage of males and females:\t {per_males:.2f}\t {per_females:.2f}')
     ##################################################
     pass
 
